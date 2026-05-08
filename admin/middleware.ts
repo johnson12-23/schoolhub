33d89880fa01badd4eb2b04ce708d9cd02411c94
Ansu T.K Johnson <ansutkjohnson@example.com>
2026-05-08 19:13:38 +0000

    Protect admin root route

--- admin/middleware.ts
+++ admin/middleware.ts
@@ -6,5 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/schoolhub-admin/:path*"]
+  matcher: ["/schoolhub-admin", "/schoolhub-admin/:path*"]
 };
